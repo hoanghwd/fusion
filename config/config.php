81fbdfe234bfb946179b1fe75ff1a84ee1c65d99
hoanghwd <33641749+hoanghwd@users.noreply.github.com>
2021-01-20 09:24:21 -0800

    Add files via upload

--- config/config.php
+++ config/config.php
@@ -7,6 +7,6 @@ error_reporting(E_ALL);
 define("BASE_URL", "https://www.huynhdo.us/fusion");
 define("HOST", "localhost");
 define("DB", "fusion");
-define("DB_USR", "root");
-define("DB_PASS", "xuongrong1976");
+define("DB_USR", "xxxxxx");
+define("DB_PASS", "xxxxxxxx");
 
